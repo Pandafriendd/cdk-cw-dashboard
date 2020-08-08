042e3198f9a9f2f6912864787b1704f0bbfa9c72
Li <lizhiyua@38f9d35d438d.ant.amazon.com>
2020-08-08 01:53:16 -0400

    first commit

--- lib/cdk-cw-dashboard-stack.ts
+++ lib/cdk-cw-dashboard-stack.ts
@@ -1,18 +1,20 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
+import * as cloudwatch from '@aws-cdk/aws-cloudwatch';
 import * as cdk from '@aws-cdk/core';
+
 
 export class CdkCwDashboardStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkCwDashboardQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const dashboard = new cloudwatch.Dashboard(this, 'test', {
+      dashboardName: 'SimpleDashboard'
     });
+    const myGraphWidget = new cloudwatch.GraphWidget({
+      height: 5,
+      width: 5
+    });
+    dashboard.addWidgets(myGraphWidget);
 
-    const topic = new sns.Topic(this, 'CdkCwDashboardTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    //Add Metric
   }
 }
